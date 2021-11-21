c16ee2ef376840154b67732e1b5834859de327a9
Humayan-Kabir <zyxwv.hk@gmail.com>
2021-11-21 22:10:16 +0600

    easy search insert position

---
+++ leetcode/easy_search_insert_position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+	int low = 0, high = nums.size() - 1, position = -1;
+	while(low <= high) {
+		int mid = (low + high) / 2;
+		if(nums[mid] < target) {
+			position = mid;
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+	position++;
+	return position;
+    }
+};
