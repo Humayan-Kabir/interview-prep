6c831a03c51acdd439f302072e6185098cd04ee7
Humayan-Kabir <zyxwv.hk@gmail.com>
2021-11-21 22:28:41 +0600

    easy search insert position

--- leetcode/maximum_subarray.cpp
+++ leetcode/maximum_subarray.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
     	int result = INT_MIN;
-	int sum = 0;
-	for(int num: nums) {
-		sum += num;
-		result = max(result, sum);
-		if(sum < 0) {
-			sum = 0;
+		int sum = 0;
+		for(int num: nums) {
+			sum += num;
+			result = max(result, sum);
+			if(sum < 0) {
+				sum = 0;
+			}
 		}
-	}
-	return result;	
+		return result;	
     }
 };
