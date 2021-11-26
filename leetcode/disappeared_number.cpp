72b3e52d5e07df549e0f6a49f9a87a6f7138daac
Humayan-Kabir <zyxwv.hk@gmail.com>
2021-11-27 00:51:44 +0600

    disappeared number solved

---
+++ leetcode/disappeared_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+      for (int i = 0; i < nums.size(); i++) {
+          int idx = abs(nums[i]) - 1;
+          if (nums[idx] > 0) {
+            nums[idx] *= -1;
+          }
+      }
+      vector<int> ans;
+      for (int i = 1; i <= n; i++) {
+        if (nums[i] > 0) {
+            ans.push_back(i);
+        }
+      }
+      return ans;
+    }
+};
