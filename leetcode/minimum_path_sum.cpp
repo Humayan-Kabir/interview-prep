ae3fce19100488816c61f54c14c27e72c1092cb2
Humayan-Kabir <zyxwv.hk@gmail.com>
2021-12-21 00:34:11 +0600

    minimum path sum solved

--- leetcode/minimum_path_sum.cpp
+++ leetcode/minimum_path_sum.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        for(int i = 1; i < grid.size(); i++) {
+        for(int i = 0; i < grid.size(); i++) {
             for(int j = 0; j < grid[0].size(); j++) {
-                int upResult = grid[i][j - 1];
+                if (i == 0 && j == 0) continue;
+                int upResult = i > 0 ? grid[i - 1][j] : 1e9;
                 int leftResult = j > 0 ? grid[i][j - 1] : 1e9;
-                grid[i][j] = min(upResult + grid[i][j], leftResult + grid[i][j]);     
+                grid[i][j] = min(upResult + grid[i][j], leftResult + grid[i][j]);             
             }
         }
         return grid[grid.size() - 1][grid[0].size() - 1];       
