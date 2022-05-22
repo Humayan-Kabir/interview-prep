5076805420fbee11780a0dad5d854da832c4c757
Humayan-Kabir <zyxwv.hk@gmail.com>
2022-05-23 02:05:21 +0600

    merged interval

---
+++ leetcode/amazon-card/mergedIntervals.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+       	vector<vector<int>> merged;
+       	for(auto it : intervals) {
+       		if (merged.empty() || merged.back()[1] < it[0]) {
+       			merged.push_back(it);
+       		} else {
+       			merged.back()[1] = max(merged.back()[1], it[1]);
+       		}
+       	}
+       	return merged;
+    }
+};
