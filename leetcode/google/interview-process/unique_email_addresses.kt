02e141258bbbd158a42fa1f4bf6ea452b909b04f
Humayan-Kabir <zyxwv.hk@gmail.com>
2022-06-12 14:05:46 +0600

    unique email addresses solved

---
+++ leetcode/google/interview-process/unique_email_addresses.kt
@@ -0,0 +1,13 @@
+class Solution {
+     fun numUniqueEmails(emails: Array<String>): Int {
+        val hashSet = HashSet<String>()
+        for (email in emails) {
+            email.split('@').also { parts ->
+                val filteredEmail = parts.first().split('+').first().replace(".", "")
+                val validDomain = parts[1]
+                hashSet.add("$filteredEmail@$validDomain")
+            }
+        }
+        return hashSet.size
+    }
+}
